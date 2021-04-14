38d0633a42986c4dd3a5b8b7e105efe10c835b4a
Mark Andrews <mjandrews.org@gmail.com>
2021-04-14 19:04:02 +0100

    Update script

--- scripts/script1.R
+++ scripts/script1.R
@@ -67,6 +67,7 @@ sum(residuals(M2)^2)
 
 M1 <- lm(Fertility ~ Agriculture + Education + Catholic, data = swiss)
 M0 <- lm(Fertility ~ Agriculture + Education, data = swiss)
+M0_alt <- lm(Fertility ~ Education + Agriculture, data = swiss)
 
 RSS_1 <- sum(residuals(M1)^2)
 RSS_0 <- sum(residuals(M0)^2)
@@ -77,3 +78,35 @@ RSS_1
 RSS_0/RSS_1
 
 (RSS_0 - RSS_1) / RSS_1
+
+df_0 <- M0$df.residual
+df_1 <- M1$df.residual
+
+(RSS_0 - RSS_1) / (df_0 - df_1)
+
+RSS_1/df_1
+
+f_stat <- ((RSS_0 - RSS_1) / (df_0 - df_1)) / (RSS_1/df_1)
+
+# p value for the null hypothesis test 
+pf(f_stat, df1 = df_0 - df_1, df2 = df_1, lower.tail = F)
+
+anova(M0, M1)
+anova(M0,M1)$`Pr(>F)`[2] # for just the p-value
+
+drop1(M1, scope = ~ Catholic, test = 'F')
+
+anova(M0)
+
+M0_drop_edu <- lm(Fertility ~ Agriculture, data = swiss)
+sum(residuals(M0_drop_edu)^2)
+M0_drop_edu_drop_agri <- lm(Fertility ~ 1, data = swiss)
+sum(residuals(M0_drop_edu_drop_agri)^2)
+
+M0
+M_null <- lm(Fertility ~ 1, data = swiss)
+RSS_null <- sum(residuals(M_null)^2)
+c(RSS_null, RSS_0, RSS_null - RSS_0)
+
+(RSS_null - RSS_0)/RSS_null
+summary(M0)$r.squared
